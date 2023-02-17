c5b4b0660ea07bbb851ca9d74d7ade0596858dbe
Donatas Drobuzas <donatas.drobuzas@gmail.com>
2023-02-17 15:24:49 +0200

    feat: Added style on housecard

--- client/src/pages/house-page/index.tsx
+++ client/src/pages/house-page/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import routes from 'navigation/routes';
-import { Box } from '@mui/material';
+import {
+  Box, Card, CardContent, CardMedia, Typography,
+} from '@mui/material';
 import useHouse from 'hooks/useHouse';
 
 const HousePage = () => {
@@ -11,8 +13,46 @@ const HousePage = () => {
   if (id === undefined) return <Navigate to={routes.HomePage} />;
 
   return (
-    <Box component="pre">
-      {JSON.stringify(house, null, 4)}
+    <Box
+      sx={{
+        maxWidth: 600,
+        mx: 'auto',
+        my: 4,
+        '@media (min-width:600px)': {
+          maxWidth: 800,
+        },
+        '@media (min-width:960px)': {
+          maxWidth: 1000,
+        },
+      }}
+    >
+      <Card>
+        <CardMedia
+          component="img"
+          height="400"
+          image={house?.images[0]}
+          alt={house?.title}
+        />
+        <CardContent>
+          <Typography variant="h4" gutterBottom>
+            {house?.title}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {house?.location.city}
+            ,
+            {' '}
+            {house?.location.country}
+          </Typography>
+          <Typography variant="h5" sx={{ mt: 2 }}>
+            {house?.price}
+          </Typography>
+          <Typography variant="h6" sx={{ mt: 2 }}>
+            Rating:
+            {' '}
+            {house?.rating}
+          </Typography>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
